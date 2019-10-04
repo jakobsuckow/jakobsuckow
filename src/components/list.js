c0af5acfd8b564d610c0623cafc47e46e75cad46
jakobsuckow <jakob@martian.agency>
2019-10-04 13:01:57 +0200

    more styling

--- src/components/list.js
+++ src/components/list.js
@@ -4,7 +4,7 @@ import { useStaticQuery, graphql } from "gatsby"
 const List = () => {
     const data = useStaticQuery(graphql`
     query listQuery {
-        allAirtable {
+        allAirtable(sort: {fields: data___Year, order: DESC}) {
           edges {
             node {
               data {
@@ -30,14 +30,16 @@ const List = () => {
       }
       
       
+      
+      
     `)
 
     return (
         <>
         {data.allAirtable.edges.map((edge, index) => (
             <ul key={index} className="project__list">
-                <li>{edge.node.data.Year}</li>
-                <li>{edge.node.data.Name}</li>
+                <li className="year">{edge.node.data.Year}</li>
+                <li className="project__name">{edge.node.data.Name}</li>
             </ul>
         ))}
         </>
