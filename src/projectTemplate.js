202d048a00a998ac8d5f4807099494f49704d1ab
jakobsuckow <jakob@martian.agency>
2019-10-05 21:40:51 +0200

    upgraded projectTemplate

--- src/projectTemplate.js
+++ src/projectTemplate.js
@@ -8,14 +8,22 @@ const projectTemplate = ({data}) => {
     return (
         <>
         <SEO
-        title="template"
+        title={`${fields.Title}`}
         />
-        <div>
+        <div className="intro">
             <h1>{fields.Title}</h1>
             <p>{fields.Description}</p>
             <p>{fields.Year}</p>
-
-            <img src={fields.Attachments[0].url} alt="asd" />
+            {fields.Attachments[0].type === 'video/mp4' ? (
+              <video controls >
+              <source src={fields.Attachments[0].url} type="video/mp4" />
+            Your browser does not support the video tag.
+            </video>
+            ): (
+              <img src={fields.Attachments[0].url} alt={fields.Title} />
+            )
+            }
+            
         </div>
         </>
     )
@@ -25,32 +33,38 @@ export default projectTemplate
 
 export const pageQuery = graphql`
 query templateQuery($title: String!) {
-    allAirtable(filter: {data: {Title: {eq: $title}}}) {
-      edges {
-        node {
-          data {
-            Description
-            Format
-            Title
-            Year
-            Attachments {
-              thumbnails {
-                large {
-                  url
-                  height
-                  width
-                }
-                full {
-                  url
-                  height
-                  width
-                }
+  allAirtable(filter: {data: {Title: {eq: $title}}}) {
+    edges {
+      node {
+        data {
+          Description
+          Format
+          Title
+          Year
+          Attachments {
+            thumbnails {
+              large {
+                url
+                height
+                width
               }
-              url
+              full {
+                url
+                height
+                width
+              }
             }
+            url
+            filename
+            type
           }
         }
       }
     }
   }
+}
+
+
+
+
 `
