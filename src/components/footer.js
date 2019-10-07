4fc1df0dfdadc5cab28a8a3a23ef79c624a8d552
jakobsuckow <jakob@martian.agency>
2019-10-07 13:41:16 +0200

    addded footer upgrades

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,20 @@ const footer = () => {
     return (
 
         <footer>
-       
+        <div class="footer-wrapper">
+            <div class="container">
+            <ul>
+                <li>Copyright &copy; Jakob Suckow</li>
+                <li>All rights reserved</li>
+                <li>Social: 
+                    Jakob Suckow
+                    Jakob.suckow94@googlemail.com
+
+                </li>
+            </ul>
+            </div>
+        </div>
         <Link to="/">
-        <h2>Jakob Suckow</h2>
         </Link>
         </footer>
         
